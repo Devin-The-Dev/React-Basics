571e437ab6dbe8e8fcbb4c25b9d5d1e3a7cbbdc6
Devin Udy <devin.udy@gmail.com>
2021-05-10 16:32:46 -0600

    More commits

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let name = "Joe";
+let lname = "Santos";
+let age = 16;
+let loggedIn = true;
+let width = 400;
+let height = 300;
+let styles = {
+    box: {
+        background: 'red',
+        color: 'white',
+        textAlign: 'center',
+        width: `${width * 3 / 2}px`,
+        height: `${height}px`,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    h1: {
+        background: 'black',
+        width: '100px',
+        height: '80px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '.8rem'
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    let showAccount = () => {
+        return (<div>You are logged in</div>)
+    }
+    return (
+        <>
+            <div
+                style={styles.box}>
+                {/* <h2>Welcome {name + ' ' + lname}</h2>
+                <h3>I'm {age * 2} years old</h3>
+                <h3>{loggedIn ? showAccount() : ''}</h3> */}
+                <h1 style={styles.h1}>Testing</h1>
+            </div>
+            <div
+                className="main-box">
+                <h1 style={styles.h1}>Testing</h1>
+            </div>
+        </>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
