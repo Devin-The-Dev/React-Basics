996a24c5daba390ba73cb92979003a5f7eb40155
Devin Udy <devin.udy@gmail.com>
2021-05-17 16:55:40 -0600

    Added props children

--- src/index.js
+++ src/index.js
@@ -1,56 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let name = "Joe";
-let lname = "Santos";
-let age = 16;
-let loggedIn = true;
-let width = 400;
-let height = 300;
-let styles = {
-    box: {
-        background: 'red',
-        color: 'white',
-        textAlign: 'center',
-        width: `${width * 3 / 2}px`,
-        height: `${height}px`,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    h1: {
-        background: 'black',
-        width: '100px',
-        height: '80px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '.8rem'
-    }
-}
-
 const App = () => {
     const printAlert = (message) => {
         alert(message)
     }
     return (
         <>
-            App
-            <Heading content="Top JavaScript Frameworks" periods={['', '.', '..', '...']}></Heading>
-            <Heading content="My Favorite Books" printAlert={printAlert}></Heading>
-            <Heading content="I love Star Wars"></Heading>
+            <Header background='black'>
+                <div>Logo</div>
+                <nav>
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                    <a href="#">link</a>
+                </nav>
+            </Header>
+
+            <Header background='red'>
+                <div>Logo</div>
+            </Header>
         </>
     )
 }
 
-const Heading = (props) => {
-    console.log(props)
+const Header = (props) => {
     return (
-        <h1
-            onClick={() => props.printAlert('Hello World!')}>
-            {props.content} {props.periods == undefined ? '' : props.periods[3]}
-        </h1>
-    );
+        <header style={{
+            background: props.background,
+            padding: '15px 20px',
+            color: 'white'
+        }}>
+            {props.children}
+
+        </header>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
